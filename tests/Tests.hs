5c270ac50937cb17d436aff0e8aa308d3aa17b6e
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-05-29 10:27:49 +0200

    [ #12 ] clean out getBinDir (didn't work on GHA haskell-ci)

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,10 +1,5 @@
 import System.Exit     ( exitWith  )
 import System.Process  ( system    )
-import System.FilePath ( (</>)     )
-
-import Paths_goldplate ( getBinDir )
 
 main :: IO ()
-main = do
-  bindir <- getBinDir
-  exitWith =<< system ("goldplate tests")
+main = exitWith =<< system ("goldplate tests")
